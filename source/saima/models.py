9b60aa8f99efcb21a66e0c7e6836a7a35d3e08fb
Hu Songtao <idisblueflash@gmail.com>
2018-09-02 21:12:03 +0800

    add description in admin

--- source/saima/models.py
+++ source/saima/models.py
@@ -3,9 +3,15 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Command(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
     command = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.description
 
