cefc04ac92131f68af72834eec89b52f3ca7a8cb
Hu Songtao <idisblueflash@gmail.com>
2018-09-02 21:08:50 +0800

    add command, category models

--- source/saima/models.py
+++ source/saima/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Command(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
+    command = models.CharField(max_length=200)
+
